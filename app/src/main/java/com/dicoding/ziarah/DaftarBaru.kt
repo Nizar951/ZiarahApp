6bf7d9313e5d8abb6e8b57470908d28c192168cd
Nizar951 <nizarmayraldo@gmail.com>
2021-07-31 14:45:15 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/ziarah/DaftarBaru.kt
@@ -0,0 +1,11 @@
+package com.dicoding.ziarah
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DaftarBaru : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daftar_baru)
+    }
+}
---
+++ app/src/main/java/com/dicoding/ziarah/ui/login/LoginActivity.kt
@@ -0,0 +1,167 @@
+package com.dicoding.ziarah.ui.login
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.dicoding.ziarah.DaftarBaru
+import com.dicoding.ziarah.R
+import com.dicoding.ziarah.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var loginViewModel: LoginViewModel
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val username = binding.username
+        val password = binding.password
+        val login = binding.login
+        val loading = binding.loading
+
+        val btndaftaremail: Button = findViewById<Button>(R.id.DaftarEmail)
+        btndaftaremail.setOnClickListener(this)
+
+        val btnmasuk: Button = findViewById<Button>(R.id.login)
+        btnmasuk.setOnClickListener(this)
+
+        val btnlogingoogle: Button = findViewById<Button>(R.id.loginGoogle)
+        btnlogingoogle.setOnClickListener(this)
+
+        val btnloginfacebook: Button = findViewById<Button>(R.id.loginfacebook)
+        btnloginfacebook.setOnClickListener(this)
+
+        loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
+            .get(LoginViewModel::class.java)
+
+        loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
+            val loginState = it ?: return@Observer
+
+            // disable login button unless both username / password is valid
+            login.isEnabled = loginState.isDataValid
+
+            if (loginState.usernameError != null) {
+                username.error = getString(loginState.usernameError)
+            }
+            if (loginState.passwordError != null) {
+                password.error = getString(loginState.passwordError)
+            }
+        })
+
+        loginViewModel.loginResult.observe(this@LoginActivity, Observer {
+            val loginResult = it ?: return@Observer
+
+            loading.visibility = View.GONE
+            if (loginResult.error != null) {
+                showLoginFailed(loginResult.error)
+            }
+            if (loginResult.success != null) {
+                updateUiWithUser(loginResult.success)
+            }
+            setResult(Activity.RESULT_OK)
+
+            //Complete and destroy login activity once successful
+            finish()
+        })
+
+        username.afterTextChanged {
+            loginViewModel.loginDataChanged(
+                username.text.toString(),
+                password.text.toString()
+            )
+        }
+
+        password.apply {
+            afterTextChanged {
+                loginViewModel.loginDataChanged(
+                    username.text.toString(),
+                    password.text.toString()
+                )
+            }
+
+            setOnEditorActionListener { _, actionId, _ ->
+                when (actionId) {
+                    EditorInfo.IME_ACTION_DONE ->
+                        loginViewModel.login(
+                            username.text.toString(),
+                            password.text.toString()
+                        )
+                }
+                false
+            }
+
+            login.setOnClickListener {
+                loading.visibility = View.VISIBLE
+                loginViewModel.login(username.text.toString(), password.text.toString())
+            }
+        }
+    }
+
+    private fun updateUiWithUser(model: LoggedInUserView) {
+        val welcome = getString(R.string.welcome)
+        val displayName = model.displayName
+        // TODO : initiate successful logged in experience
+        Toast.makeText(
+            applicationContext,
+            "$welcome $displayName",
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    private fun showLoginFailed(@StringRes errorString: Int) {
+        Toast.makeText(applicationContext, errorString, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onClick(p: View?) {
+        when (p?.id){
+            R.id.DaftarEmail -> {
+                val movedaftar = Intent (this@LoginActivity, DaftarBaru::class.java)
+                startActivity(movedaftar)
+            }
+
+            R.id.login -> {
+
+            }
+
+            R.id.loginfacebook -> {
+
+            }
+
+            R.id.loginGoogle -> {
+
+            }
+        }
+    }
+
+
+}
+
+/**
+ * Extension function to simplify setting an afterTextChanged action to EditText components.
+ */
+fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(editable: Editable?) {
+            afterTextChanged.invoke(editable.toString())
+        }
+
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+    })
+}
